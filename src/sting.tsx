86ebbc28c4b9decc85f9014d9d4fe3d9da672cfc
Richard Jones <richard.jones.dev@gmail.com>
2025-03-14 23:21:23 +0000

    Initial commit

---
+++ src/sting.tsx
@@ -0,0 +1,54 @@
+import { FC, useCallback, useEffect, useMemo, useState } from "react";
+import styles from './sting.module.css'
+import { useStopwatch } from "react-timer-hook";
+import classNames from "classnames";
+
+const times = [5, 15, 30, 35];
+
+export const StingComponent: FC = () => {
+    const [timeIndex, setTimeIndex] = useState<number>(0);
+    const [backgroundActivated, setBackgroundActivated] = useState<boolean>(false);
+    let {totalSeconds, hours, minutes, seconds, pause, start, isRunning} = useStopwatch({autoStart: true});
+
+      const toggleSting = useCallback(() => {
+        if (!backgroundActivated) {
+          setBackgroundActivated(true);
+          return;
+        }
+        setBackgroundActivated(false);
+      }, [backgroundActivated, setBackgroundActivated]);
+
+      const toggleTimeRunningState = useCallback(() => {
+        if (isRunning) {
+          pause();
+          return;
+        }
+        start();
+      }, [pause, start, isRunning])
+
+      useEffect(() => {
+        if (totalSeconds === times[timeIndex]) {
+          toggleSting();
+          setTimeIndex((oldVal) => oldVal + 1);
+        }
+      }, [totalSeconds, setTimeIndex, timeIndex]);
+
+      const resetTimer = useCallback(() => {
+      }, [totalSeconds, hours, minutes, seconds, pause, start, isRunning]);
+
+    return (
+    <div className={styles.stingAppContainer}>
+      <div className={styles.stingContainer}>
+        <div className={styles.stingSword}></div>
+        <div className={backgroundActivated ? classNames(styles.stingGlow, styles.stingGlowUp) : classNames(styles.stingGlow, styles.stingGlowDown)}></div>  
+      </div>
+      <div>
+        <span>{hours}:{minutes}.{seconds}</span>
+      </div>
+      <div>
+        <button onClick={() => toggleSting()}>Test</button>
+        <button onClick={() => {toggleTimeRunningState()}}>{isRunning ? "Pause" : "Play"}</button>
+        <button onClick={() => {resetTimer()}}>Reset</button>
+      </div>
+    </div>)
+}
