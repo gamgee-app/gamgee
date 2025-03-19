5f9eface4fddd284c97af488c594d01ef9b482cf
Oliver Broomhall <obroomhall@gmail.com>
2025-03-19 19:16:21 +0000

    `npx prettier .\src\ --write`

--- src/movies.ts
+++ src/movies.ts
@@ -1,5 +1,5 @@
 export const movies = {
-    tt0120737: 'The Lord of the Rings: The Fellowship of the Ring',
-    tt0167261: 'The Lord of the Rings: The Two Towers',
-    tt0167260: 'The Lord of the Rings: The Return of the King'
-}
+  tt0120737: "The Lord of the Rings: The Fellowship of the Ring",
+  tt0167261: "The Lord of the Rings: The Two Towers",
+  tt0167260: "The Lord of the Rings: The Return of the King",
+};
