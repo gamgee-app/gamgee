237932b053adf676fbcda07906e962534ab6df43
Oliver Broomhall <obroomhall@gmail.com>
2025-03-19 19:30:15 +0000

    Add editions and durations to movies.ts

---
+++ src/movies/movies.ts
@@ -0,0 +1,14 @@
+export const movies = {
+  tt0120737: {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    editions: [{ theatrical: 10705472 }, { extended: 13691758 }],
+  },
+  tt0167261: {
+    title: "The Lord of the Rings: The Two Towers",
+    editions: [{ theatrical: 10765760 }, { extended: 14125166 }],
+  },
+  tt0167260: {
+    title: "The Lord of the Rings: The Return of the King",
+    editions: [{ theatrical: 12061823 }, { extended: 15790797 }],
+  },
+};
