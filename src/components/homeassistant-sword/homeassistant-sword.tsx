143bfb5e4e6f236ddc353bb8c4f41e9937226b6f
Richard Jones <richard.jones.dev@gmail.com>
2025-10-04 20:03:27 +0100

    change to fetchApi

--- src/components/homeassistant-sword/homeassistant-sword.tsx
+++ src/components/homeassistant-sword/homeassistant-sword.tsx
@@ -5,18 +5,16 @@ interface HomeAssistantSwordProps {
 }
 
 export const HomeAssistantSword : FC<HomeAssistantSwordProps> = ({swordIsGlowing}) => {
-    const xhr = new XMLHttpRequest();
-
     useEffect(() => {
         if (swordIsGlowing) {
-            xhr.open('POST', 'http://192.168.1.140:8123/api/webhook/sword_on');
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.send();
+            fetch('http://192.168.1.140:8123/api/webhook/sword_on', 
+                {method: 'POST'}
+            );
         }
         else{
-            xhr.open('POST', 'http://192.168.1.140:8123/api/webhook/sword_off');
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.send();
+            fetch('http://192.168.1.140:8123/api/webhook/sword_on', 
+                {method: 'POST'}
+            );
         }
     }, [swordIsGlowing]);
 
